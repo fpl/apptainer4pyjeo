29a7e059123df91615913f980a785ad5a382268b
Francesco Paolo Lovergine <frankie@debian.org>
2024-08-08 09:37:55 +0200

    Introduced changes for the SBSC environment for its HPC cluster.

--- install_pyjeo4apptainer.sh
+++ install_pyjeo4apptainer.sh
@@ -1,18 +1,36 @@
 #!/bin/sh
 #
-# This script installs an apptainer container for PyJeo use.
+# This script installs an apptainer container for PyJeo use in SBSC HPC cluster
 #
 
-rm -rf $HOME/pyjeo $HOME/pyjeo-install
-mkdir $HOME/pyjeo
-( cd $HOME/pyjeo \
+BASE=/gpfs/gibbs/project/sbsc/hydro/pyjeo-stuff
+
+mkdir $BASE || true
+
+rm -rf $BASE/pyjeo $BASE/pyjeo-install
+mkdir $BASE/pyjeo
+
+( cd $BASE/pyjeo \
 && curl -L --output mialib.tar.gz https://github.com/ec-jrc/jeolib-miallib/archive/refs/tags/v1.1.1.tar.gz \
 && curl -L --output jiplib.tar.gz https://github.com/ec-jrc/jeolib-jiplib/archive/refs/tags/v1.1.3.tar.gz \
 && curl -L --output pyjeo.tar.gz https://github.com/ec-jrc/jeolib-pyjeo/archive/refs/tags/v1.1.7.tar.gz \
 )
-apptainer build $HOME/pyjeo1.sif $HOME/pyjeo1.def
-apptainer build $HOME/pyjeo2.sif $HOME/pyjeo2.def
-apptainer run $HOME/pyjeo2.sif $HOME/pyjeo2.def
 
-# from now on run
-# apptainer exec $HOME/pyjeo2.sif python3 ... 
+cp -v pyjeo*.def $BASE/.
+
+( cd $BASE \
+apptainer build $BASE/pyjeo1.sif $BASE/pyjeo1.def \
+apptainer build $BASE/pyjeo2.sif $BASE/pyjeo2.def \
+apptainer run --bind /gpfs/gibbs/project/sbsc/hydro $BASE/pyjeo2.sif $BASE/pyjeo2.def \
+)
+
+if ! grep PYJEO_BASE $HOME/.bashrc
+then
+cat >$HOME/.bashrc <<EOF
+#--- Begin stuff added to run PyJeo in apptainer ---
+export PYJEO_BASE=$BASE
+[ -f \$PYJEO_BASE/pyjeovenv/bin/activate ] && source \$PYJEO_BASE/pyjeovenv/bin/activate
+export PYJEO_RUN="apptainer exec --bind /gpfs/gibbs/project/sbsc/hydro --bind /gpfs/gibbs/pi/hydro/hydro --bind /vast/palmer/scratch/sbsc/hydro \$PYJEO_BASE/pyjeo2.sif python3"
+#--- End ---
+EOF
+fi
